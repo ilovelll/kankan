25be7e30daca2d7aa671ad2dd4c485b1e905ba9f
lihongen <358505321@qq.com>
2015-08-21 09:59:40 +0800

    修改api文档

--- apiDoc/apidoc.js
+++ apiDoc/apidoc.js
@@ -1,5 +1,5 @@
 /**
- * @api {post} /register 注册
+ * @api {post} /users/register 注册
  * @apiName Register
  * @apiPermission none
  * @apiDescription 注册新用户
@@ -8,7 +8,7 @@
  * @apiParam {String} pwd 密码
  * @apiParam {String} geohash 用户地理位置信息
  * @apiExample {curl} 接口示例：
- *   curl -d "name=lihongen&pass=ffff&geohash=ww0y51rcstxn" http://http://ubuntu-redred.myalauda.cn/register
+ *   curl -d "name=lihongen&pass=ffff&geohash=ww0y51rcstxn" http://http://ubuntu-redred.myalauda.cn/users/register
  * @apiSuccess {Boolean} success 注册成功与否
  * @apiSuccess {Number} uid 返回用户注册id
  * @apiSuccess {String} msg 返回接口请求信息
@@ -21,13 +21,13 @@
  */
 
 /**
- * @api {post} /token 获取token值
+ * @api {post} /users/token 获取token值
  * @apiName GetToken
  * @apiPermission none
  * @apiGroup User
  * @apiDescription 获取校验token，用来接口请求，有效期五天
  * @apiExample {curl} 接口示例：
- *   curl -d "uid=7&pwd=ffff" http://http://ubuntu-redred.myalauda.cn/token
+ *   curl -d "uid=7&pwd=ffff" http://http://ubuntu-redred.myalauda.cn/users/token
  * @apiSuccess {Boolean} success 请求成功与否
  * @apiSuccess {String} token token值
  * @apiSuccess {String} msg 返回接口请求信息
